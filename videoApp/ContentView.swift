80d3b219dae0f6b840540421648fad257c9b6bfe
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-11-03 09:28:28 +0900

    first commit

---
+++ videoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  videoApp
+//
+//  Created by Miyuki Kikuchi on 03/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ videoApp/videoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  videoAppApp.swift
+//  videoApp
+//
+//  Created by Miyuki Kikuchi on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct videoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
